89f32d03d54a29cc3263a518036f51035bf50979
Ryan Niño Dizon <50763950+ryanninodizon@users.noreply.github.com>
2024-06-24 10:54:55 +0200

    Update http-ryan.interceptor.ts

--- frontend-angular/src/app/http-ryan.interceptor.ts
+++ frontend-angular/src/app/http-ryan.interceptor.ts
@@ -5,8 +5,7 @@ export const httpRyanInterceptor: HttpInterceptorFn = (req, next) => {
   //Get the access token from localStorageSession -- Temporary implementation
   let token = localStorage.getItem("00000000-0000-0000-84d1-95697bafc8a6.9188040d-6c67-4c5b-b112-36a304b66dad-login.windows.net-idtoken-c1eed0b4-a59d-46bd-b3ee-416e35abbfa3-3e11228a-56f5-450d-9cf1-6283bdb7f12c---"); 
   let secret = token?.split(",")[4].split(":")[1].toString();
-  secret = secret?.substring(1, secret.length - 1);
-  
+  secret = secret?.substring(1, secret.length - 1);  
   const authToken = secret; 
   // Clone the request and add the authorization header
   const authReq = req.clone({
@@ -14,7 +13,6 @@ export const httpRyanInterceptor: HttpInterceptorFn = (req, next) => {
       Authorization: `Bearer ${authToken}`
     }
   });
-
   return next(authReq).pipe(
     catchError((err: any) => {
       if (err instanceof HttpErrorResponse) {
@@ -31,7 +29,6 @@ export const httpRyanInterceptor: HttpInterceptorFn = (req, next) => {
         // Handle non-HTTP errors
         console.error('An error occurred:', err);
       }
-
       // Re-throw the error to propagate it further
       return throwError(() => err); 
     })
