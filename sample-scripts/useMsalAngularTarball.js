41c61cd7f6d42647e5410532f6fbc0cc21ccf4db
Ryan Niño Dizon <50763950+ryanninodizon@users.noreply.github.com>
2024-07-02 12:50:59 +0200

    Update useMsalAngularTarball.js

--- sample-scripts/useMsalAngularTarball.js
+++ sample-scripts/useMsalAngularTarball.js
@@ -14,15 +14,11 @@ const angularSamplePath = process.cwd();
 
 // Build MSAL Angular
 execSync('npm run build', { cwd: msalAngularPath, stdio: 'inherit' });
-
 // Run npm pack
 execSync('npm pack', { cwd: msalAngularDistPath, stdio: 'inherit' });
-
 // Read MSAL Angular package.json
 const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
-
 // Construct the filename of the .tgz file
 const tgzFilename = `azure-msal-angular-${packageJson.version}.tgz`;
-
 // Install the .tgz file
 execSync(`npm i ${path.join(msalAngularDistPath, tgzFilename)}`, { cwd: angularSamplePath, stdio: 'inherit' });
