b6476f5a15a44e23ad756d5f85816c8f902d5554
Ryan Niño Dizon <50763950+ryanninodizon@users.noreply.github.com>
2024-09-20 09:15:03 +0200

    Update CosmosDbConfig.cs

--- backend-azure-functions/CosmosDbConfig.cs
+++ backend-azure-functions/CosmosDbConfig.cs
@@ -3,7 +3,7 @@ namespace Serverless.Api
     public static class CosmosDbConfig{
         public static readonly string DbPrimaryKey = "<CosmosDb Primary Key>";
         public static readonly string DbName = "<your cosmos-db-name>";
-        public static readonly string DbContainerName = "your <cosmos-db-container-name>";
+        public static readonly string DbContainerName = "<your cosmos-db-container-name>";
         public static readonly string DbEndpointUri = "<CosmosDb Endpoint URI>";
     }
 }
